4059a448841087fb429cd5c0de40a230c60ae8e0
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-01-21 16:27:56 +0530

    Add functions to count characters and remove spaces

--- practice.js
+++ practice.js
@@ -28,3 +28,42 @@ function isPalindrome(str){
 
 console.log(isPalindrome("madam")); // true
 console.log(isPalindrome("world")); // false
+
+
+3.Count Character Occurrences
+
+Problem:
+Count how many times a specific character appears in a string.
+
+Example:
+Input: "banana", "a" → Output: 3
+
+
+function countChar(str, char){
+    let count=0
+    for(let i=0; i<str.length; i++){
+        if(str[i]=== char){
+            count++
+        }
+    }
+    return count
+}
+
+console.log(countChar("hello", "l")); // 2
+console.log(countChar("banana", "a")); // 3 
+console.log(countChar("mississippi", "s")); // 4
+
+
+4.Remove all spaces from a string.
+
+function removeSpaces(str){
+    let result = ""
+    for(let i of str){
+        if(i!==" "){
+            result+=i
+        }
+    }
+    return result
+}
+
+console.log(removeSpaces("loop method works")); // "loopmethodworks"
