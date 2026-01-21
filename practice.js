5ae71a26d7ae1876bf07c61ebba6128dbef019b0
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-01-21 15:34:34 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -13,4 +13,18 @@ return arr.join('')
 }
 console.log(reversed("hello"))
 
-2.
+2.Check if a given string is a palindrome
+
+function isPalindrome(str){
+    let left=0
+    let right= str.length -1
+    while (left < right ){
+        if( str[left] !== str[right]){
+            return false;
+        }
+        return true;
+    }
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("world")); // false
