67955a8d6ef9d8dd2b29ef780e4f1c828c36c203
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-01-21 15:07:52 +0530

    Add string reversal function
    Implement a function to reverse a given string.

--- practice.js
+++ practice.js
@@ -1,1 +1,16 @@
+1. Write a function that takes a string and returns the reversed string.
 
+  function reversed(str){
+    let arr= str.split('')
+left =0
+right = arr.length -1
+while (left < right){
+    [arr[left], arr[right]] = [arr[right], arr[left]]
+    left++;
+    right--;
+}
+return arr.join('')
+}
+console.log(reversed("hello"))
+
+2.
