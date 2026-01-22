f24fe85ec5ac9cedf02df84a13bfcf2eef90dac5
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-01-22 17:52:25 +0530

    Implement first and last occurrence binary search
    Added functions to find the first and last occurrences of a target in a sorted array using binary search.

--- practice.js
+++ practice.js
@@ -174,6 +174,7 @@ console.log(binarySearch(sortedArr, 7)); // Output: 3
 console.log(binarySearch(sortedArr, 4)); // Output: -1
 
 7. Binary Search that finds the first occurrence of a target in a sorted array
+Even after finding element, search left side
  
  function firstOccurrence(sortedArr, target){
      let left=0
@@ -196,3 +197,28 @@ console.log(binarySearch(sortedArr, 4)); // Output: -1
 const sortedArr = [1, 3, 5,3, 7,3, 9, 11]; 
 console.log(firstOccurrence(sortedArr, 3)); // Output: 3 
 console.log(firstOccurrence(sortedArr, 4)); // Output: -1
+
+8.Last Occurrence of Element
+After finding element, search right side
+
+ function lastOccurrence(sortedArr, target){
+     let left=0
+     let right =sortedArr.length -1
+     let result= -1
+     while (  left <= right){
+         let mid = Math.floor((left+right) / 2)
+         if(sortedArr[mid] === target){
+             result = mid
+             left = mid +1
+         }else if(sortedArr[mid] < target){
+             left = mid +1
+         }else{
+             right = mid -1
+         }
+           
+     }
+     return result; 
+ }
+const sortedArr = [1, 3, 5,3, 7,3, 9, 11]; 
+console.log(lastOccurrence(sortedArr, 3)); // Output: 3 
+console.log(lastOccurrence(sortedArr, 4)); // Output: -1
