f6eb5a1f4c9933f9e87b226a111057cc593e6db2
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-01-22 17:47:05 +0530

    Add function to find first occurrence in sorted array

--- practice.js
+++ practice.js
@@ -172,3 +172,27 @@ console.log(arr)
 const sortedArr = [1, 3, 5, 7, 9, 11]; 
 console.log(binarySearch(sortedArr, 7)); // Output: 3 
 console.log(binarySearch(sortedArr, 4)); // Output: -1
+
+7. Binary Search that finds the first occurrence of a target in a sorted array
+ 
+ function firstOccurrence(sortedArr, target){
+     let left=0
+     let right =sortedArr.length -1
+     let result= -1
+     while (  left <= right){
+         let mid = Math.floor((left+right) / 2)
+         if(sortedArr[mid] === target){
+             result = mid
+             right = mid -1 
+         }else if(sortedArr[mid] < target){
+             left = mid +1
+         }else{
+             right = mid -1
+         }
+           
+     }
+     return result; 
+ }
+const sortedArr = [1, 3, 5,3, 7,3, 9, 11]; 
+console.log(firstOccurrence(sortedArr, 3)); // Output: 3 
+console.log(firstOccurrence(sortedArr, 4)); // Output: -1
