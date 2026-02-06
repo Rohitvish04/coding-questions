2c8f2a97d10482e5ff46cdddf5ec098d339cceb2
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2026-02-06 19:19:44 +0530

    Is Subsequence add

--- practice.js
+++ practice.js
@@ -418,3 +418,21 @@ function productExceptSelf(nums) {
 nums = [1, 2, 3, 4]
 console.log(productExceptSelf(nums))
 
+15. Is Subsequence
+
+function isSubsequence(s,t ){
+     let i=0, j=0;
+     while (i < s.length && j < t.length ) {
+         if (s[i] === t[j]){
+             i++;
+         }
+         j++
+     }
+     return i === s.length ;
+}
+
+console.log(isSubsequence("abc", "ahbgdc")); // true
+console.log(isSubsequence("axc", "ahbgdc")); // false
+console.log(isSubsequence("", "hello"));     // true
+
+
